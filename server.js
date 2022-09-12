e8ac5289f2743229580d5ae6e54f1942994f568d
kholiavkoi <kholiavkoi@gmail.com>
2022-09-12 20:42:56 +0300

    serv

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./public/db.json', {
-    static: './build'
-})
+const router = jsonServer.router('./public/db.json')
 const middlewares = jsonServer.defaults({
     static: './build'
 })
