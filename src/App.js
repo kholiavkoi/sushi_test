0847706eee0a6f98082819941eb01c33dc6dca11
kholiavkoi <kholiavkoi@gmail.com>
2022-09-12 21:46:51 +0300

    fixed layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {Header} from "./components";
 import {Cart, Home} from "./pages";
 import {Route, Routes} from "react-router-dom";
